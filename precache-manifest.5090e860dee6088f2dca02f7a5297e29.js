263d33ca91e28b73122839445b17802b34199d10
Alex <alexbibig@gmail.com>
2020-09-12 14:53:09 +0300

    Updates

---
+++ precache-manifest.5090e860dee6088f2dca02f7a5297e29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dae18d26caa541537f8ac41279f2c07",
+    "url": "/TMS-React-Rick-Morty-DataBase/index.html"
+  },
+  {
+    "revision": "3364a68d1016c82e10eb",
+    "url": "/TMS-React-Rick-Morty-DataBase/static/css/main.537546ba.chunk.css"
+  },
+  {
+    "revision": "08f1af0e9f489ee06099",
+    "url": "/TMS-React-Rick-Morty-DataBase/static/js/2.06f3edc8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TMS-React-Rick-Morty-DataBase/static/js/2.06f3edc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3364a68d1016c82e10eb",
+    "url": "/TMS-React-Rick-Morty-DataBase/static/js/main.cadfc50b.chunk.js"
+  },
+  {
+    "revision": "4b863eebf38825955b09",
+    "url": "/TMS-React-Rick-Morty-DataBase/static/js/runtime-main.b8ead4e4.js"
+  }
+]);
